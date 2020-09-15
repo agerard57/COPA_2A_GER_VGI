64f9031de0fafaac44cf4996440b6cf81429c0c9
GERARD Alexandre <56207146+agerard57@users.noreply.github.com>
2020-09-16 00:33:10 +0200

    Quick implementation of a console menu

--- src/Main_app/Main.java
+++ src/Main_app/Main.java
@@ -1,5 +1,6 @@
 package Main_app;
 
+import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -7,6 +8,48 @@ import java.sql.Statement;
 
 public class Main {
  
-	//Hello World
+	public static void main(String[] args) {
+		
+			System.out.println("Bienvenue dans la boutique des meilleurs pires pulls !");
+			
+			int selection = 0;
+			
+			do
+			{
+				System.out.println("[1] Choix du produit");
+				System.out.println("[2] Gestion de la commande");
+				System.out.println("[3] Paramètres clients");
+				System.out.println("[4] Quitter");	
+				
+			System.out.println("Utilisez le clavier numérique pour faire votre choix...");
+			Scanner LireConsole = new Scanner(System.in);
+			selection = LireConsole.nextInt();
+				
+				
+				switch (selection)
+				{
+				
+				case 1 :
+					test.choix1();
+					break;
+				case 2 :
+					test.choix2();;
+					break;				
+				case 3 :
+					test.choix3();
+					break;				
+				case 4 :
+					System.out.println("----------------");
+					System.exit(0);
+					break;
+					
+				default :
+					System.out.println ("La séléction est incorrecte !");
+					}
+				
+				
+			} while (selection != 4);
+			
 
-}
+			
+}}
---
+++ src/Main_app/test.java
@@ -0,0 +1,20 @@
+package Main_app;
+
+public class test {
+	
+	static public void choix1() 
+	{
+		System.out.println("Vous avez choisi le 1er choix");
+	}
+
+	static public void choix2() 
+	{
+		System.out.println("Vous avez choisi le 2ème choix");
+	}
+
+	static public void choix3() 
+	{
+		System.out.println("Vous avez choisi le 3ème choix");
+	}
+
+}
