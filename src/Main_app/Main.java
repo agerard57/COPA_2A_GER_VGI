dd5b04afa2b6cf0a047d5c5cea76f3e1ee623e44
GERARD Alexandre <56207146+agerard57@users.noreply.github.com>
2020-10-11 16:35:45 +0200

    Working menu

--- src/Main_app/Main.java
+++ src/Main_app/Main.java
@@ -1,8 +1,24 @@
 package Main_app;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
+public class Main extends Application{
+	
+	@Override
+	public void start(Stage stage) throws Exception{
+		Parent root = FXMLLoader.load(this.getClass().getResource("/menu.fxml"));
+		
+		stage.setScene(new Scene (root));
+		stage.show();
+}
+	
+	public static void Main(String[] args){
+		launch(Main.class, args);
 		Menu.MenuAccueil();
 
 
